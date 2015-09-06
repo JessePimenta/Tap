96298f29de0707d23c72217cfc3193c0de30a486
Jesse Pimenta <drrrreams@gmail.com>
2015-09-05 20:22:03 -0600

    added new injections into authservice

--- app.js
+++ app.js
@@ -1,71 +1,147 @@
-(function(){
-function authInterceptor(API, auth) {
-  return {
-    // automatically attach Authorization header
-    request: function(config) {
-      return config;
-    },
+(function () {
+    'use strict';
 
-    // If a token was sent back, save it
-    response: function(res) {
-      return res;
-    },
-  }
-}
+    angular
+        .module('app')
+        .factory('AuthenticationService', AuthenticationService);
 
-function authService($window) {
-  var self = this;
+    AuthenticationService.$inject = ['$http', '$cookieStore', '$rootScope', '$timeout', 'UserService'];
+    function AuthenticationService($http, $cookieStore, $rootScope, $timeout, UserService) {
+        var service = {};
 
-  // Add JWT methods here
-}
+        service.Login = Login;
+        service.SetCredentials = SetCredentials;
+        service.ClearCredentials = ClearCredentials;
 
-function userService($http, API, auth) {
-  var self = this;
-  self.getQuote = function() {
-    return $http.get(API + '/auth/quote')
-  }
+        return service;
 
-  // add authentication methods here
+        function Login(username, password, callback) {
 
-}
+            /* Dummy authentication for testing, uses $timeout to simulate api call
+             ----------------------------------------------*/
+            $timeout(function () {
+                var response;
+                UserService.GetByUsername(username)
+                    .then(function (user) {
+                        if (user !== null && user.password === password) {
+                            response = { success: true };
+                        } else {
+                            response = { success: false, message: 'Username or password is incorrect' };
+                        }
+                        callback(response);
+                    });
+            }, 1000);
 
-// We won't touch anything in here
-function MainCtrl(user, auth) {
-  var self = this;
+            /* Use this for real authentication
+             ----------------------------------------------*/
+            //$http.post('/api/authenticate', { username: username, password: password })
+            //    .success(function (response) {
+            //        callback(response);
+            //    });
 
-  function handleRequest(res) {
-    var token = res.data ? res.data.token : null;
-    if(token) { console.log('JWT:', token); }
-    self.message = res.data.message;
-  }
+        }
 
-  self.login = function() {
-    user.login(self.username, self.password)
-      .then(handleRequest, handleRequest)
-  }
-  self.register = function() {
-    user.register(self.username, self.password)
-      .then(handleRequest, handleRequest)
-  }
-  self.getQuote = function() {
-    user.getQuote()
-      .then(handleRequest, handleRequest)
-  }
-  self.logout = function() {
-    auth.logout && auth.logout()
-  }
-  self.isAuthed = function() {
-    return auth.isAuthed ? auth.isAuthed() : false
-  }
-}
+        function SetCredentials(username, password) {
+            var authdata = Base64.encode(username + ':' + password);
 
-angular.module('app', [])
-.factory('authInterceptor', authInterceptor)
-.service('user', userService)
-.service('auth', authService)
-.constant('API', 'http://test-routes.herokuapp.com')
-.config(function($httpProvider) {
-  $httpProvider.interceptors.push('authInterceptor');
-})
-.controller('Main', MainCtrl)
+            $rootScope.globals = {
+                currentUser: {
+                    username: username,
+                    authdata: authdata
+                }
+            };
+
+            $http.defaults.headers.common['Authorization'] = 'Basic ' + authdata; // jshint ignore:line
+            $cookieStore.put('globals', $rootScope.globals);
+        }
+
+        function ClearCredentials() {
+            $rootScope.globals = {};
+            $cookieStore.remove('globals');
+            $http.defaults.headers.common.Authorization = 'Basic ';
+        }
+    }
+
+    // Base64 encoding service used by AuthenticationService
+    var Base64 = {
+
+        keyStr: 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/=',
+
+        encode: function (input) {
+            var output = "";
+            var chr1, chr2, chr3 = "";
+            var enc1, enc2, enc3, enc4 = "";
+            var i = 0;
+
+            do {
+                chr1 = input.charCodeAt(i++);
+                chr2 = input.charCodeAt(i++);
+                chr3 = input.charCodeAt(i++);
+
+                enc1 = chr1 >> 2;
+                enc2 = ((chr1 & 3) << 4) | (chr2 >> 4);
+                enc3 = ((chr2 & 15) << 2) | (chr3 >> 6);
+                enc4 = chr3 & 63;
+
+                if (isNaN(chr2)) {
+                    enc3 = enc4 = 64;
+                } else if (isNaN(chr3)) {
+                    enc4 = 64;
+                }
+
+                output = output +
+                    this.keyStr.charAt(enc1) +
+                    this.keyStr.charAt(enc2) +
+                    this.keyStr.charAt(enc3) +
+                    this.keyStr.charAt(enc4);
+                chr1 = chr2 = chr3 = "";
+                enc1 = enc2 = enc3 = enc4 = "";
+            } while (i < input.length);
+
+            return output;
+        },
+
+        decode: function (input) {
+            var output = "";
+            var chr1, chr2, chr3 = "";
+            var enc1, enc2, enc3, enc4 = "";
+            var i = 0;
+
+            // remove all characters that are not A-Z, a-z, 0-9, +, /, or =
+            var base64test = /[^A-Za-z0-9\+\/\=]/g;
+            if (base64test.exec(input)) {
+                window.alert("There were invalid base64 characters in the input text.\n" +
+                    "Valid base64 characters are A-Z, a-z, 0-9, '+', '/',and '='\n" +
+                    "Expect errors in decoding.");
+            }
+            input = input.replace(/[^A-Za-z0-9\+\/\=]/g, "");
+
+            do {
+                enc1 = this.keyStr.indexOf(input.charAt(i++));
+                enc2 = this.keyStr.indexOf(input.charAt(i++));
+                enc3 = this.keyStr.indexOf(input.charAt(i++));
+                enc4 = this.keyStr.indexOf(input.charAt(i++));
+
+                chr1 = (enc1 << 2) | (enc2 >> 4);
+                chr2 = ((enc2 & 15) << 4) | (enc3 >> 2);
+                chr3 = ((enc3 & 3) << 6) | enc4;
+
+                output = output + String.fromCharCode(chr1);
+
+                if (enc3 != 64) {
+                    output = output + String.fromCharCode(chr2);
+                }
+                if (enc4 != 64) {
+                    output = output + String.fromCharCode(chr3);
+                }
+
+                chr1 = chr2 = chr3 = "";
+                enc1 = enc2 = enc3 = enc4 = "";
+
+            } while (i < input.length);
+
+            return output;
+        }
+    };
+
 })();
